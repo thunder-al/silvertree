3d89bb773de0bc6afe10a32f39529e05c17afd2a
ThunderAl <work@thunderal.net>
2023-10-01 07:47:07 +0300

    Add vite build

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,3 +1,33 @@
-import {defineConfig} from 'vitest/config'
+import {defineConfig, UserConfig} from 'vite'
+import dts from 'vite-plugin-dts'
+import nodeExternals from 'rollup-plugin-node-externals'
+import checker from 'vite-plugin-checker'
 
-export default defineConfig({})
+export default defineConfig({
+  base: './',
+  build: {
+    minify: process.env.MODE === 'production',
+    sourcemap: true,
+    lib: {
+      entry: {index: './src/index.ts'},
+      formats: ['es', 'cjs', 'umd'],
+      name: 'index',
+    },
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        sourcemapExcludeSources: true,
+      },
+      plugins: [
+        nodeExternals(),
+      ],
+    },
+  },
+  esbuild: {
+    keepNames: true,
+  },
+  plugins: [
+    dts({rollupTypes: true}),
+    checker({typescript: {buildMode: false}}),
+  ],
+})
