2e6465a5b3f59baba77b9592cdcde191c3f1abea
ThunderAl <work@thunderal.net>
2023-10-25 03:59:51 +0300

    Update rollup config

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,42 +1,41 @@
-import {defineConfig} from 'vite'
+import {defineConfig, UserConfig} from 'vite'
 import dts from 'vite-plugin-dts'
-import nodeExternals from 'rollup-plugin-node-externals'
 import checker from 'vite-plugin-checker'
+import * as fs from 'node:fs/promises'
 
-export default defineConfig({
-  plugins: [qwe() as any],
-})
 
-function qwe() {
-  return {
-    name: 'test',
-    config: () => ({
-      base: './',
-      build: {
-        minify: process.env.NODE_ENV === 'production',
-        sourcemap: true,
-        lib: {
-          entry: {index: './src/index.ts'},
-          formats: ['es', 'cjs', 'umd'],
-          name: 'index',
-        },
-        target: 'esnext',
-        rollupOptions: {
-          output: {
-            sourcemapExcludeSources: true,
-          },
-          plugins: [
-            nodeExternals(),
-          ],
-        },
+export default defineConfig(async () => {
+  const pkg = JSON.parse(await fs.readFile('./package.json', 'utf-8'))
+
+  const externals = [
+    ...Object.keys(pkg.dependencies),
+    ...Object.keys(pkg.peerDependencies),
+  ]
+
+  return <UserConfig>{
+    base: './',
+    build: {
+      minify: process.env.NODE_ENV === 'production',
+      sourcemap: true,
+      lib: {
+        entry: {index: './src/index.ts'},
+        formats: ['es', 'cjs', 'umd'],
+        name: 'index',
       },
-      esbuild: {
-        minifyIdentifiers: false,
+      target: 'esnext',
+      rollupOptions: {
+        output: {
+          sourcemapExcludeSources: true,
+        },
+        external: externals,
       },
-      plugins: [
-        dts({rollupTypes: true}),
-        checker({typescript: {buildMode: true}}),
-      ],
-    }),
+    },
+    esbuild: {
+      minifyIdentifiers: false,
+    },
+    plugins: [
+      dts({rollupTypes: true}),
+      checker({typescript: true}),
+    ],
   }
-}
+})
